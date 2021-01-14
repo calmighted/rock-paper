f389a7f99fe84a2ae7ba022232a86463178ae3aa
calmighted <calmighted@gmail.com>
2021-01-14 11:25:22 +0530

    fixed results board

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ function printBtn(e){
     updateScore(tarr.id,compChoice.id)
     addScoreToScreen()
     updateResult()
+    // determineWinner()
 
 }
 
@@ -121,7 +122,7 @@ function decideSymbol(){
     }else if (compChoice.id == "scissors"){
         compChoice.textContent = "✂️";
     }
-    console.log("AI choice: " + compChoice.id)
+    // console.log("AI choice: " + compChoice.id)
     computerBoard.appendChild(compChoice)
 }
 
@@ -146,53 +147,83 @@ function addScoreToScreen(){
     scoreBoard.appendChild(ScoreDisplayAi)
     
 }
+let result = ""
+let res = document.createElement('p')
 
 function updateResult(){
-    let res = document.createElement('p')
     res.classList.add('resText')
-    let result = ""
     res.textContent = result
+    // console.log(res.textContent)
     resultBoard.appendChild(res)
 }
+
+let refreshBtn = document.createElement('button')
+refreshBtn.textContent = 'PLAY AGAIN'
+refreshBtn.classList.add('refreshBtn')
+refreshBtn.onclick = () => window.location.reload();
 
 function updateScore(playerchoice,computerChoice){
     // playerchoice = playerchoice.toLowerCase()
 
-    if (playerchoice == computerChoice){
-        result =  "Its a tie haha!"
+    if (pp == 5){
+        res.setAttribute('id','winner')
+        result = "You Won yay! 🤩"
+        resultBoard.appendChild(refreshBtn)
+    }else if (cp == 5){
+        res.setAttribute('id','winner')
+        result = "You loose today! 😭"
+        resultBoard.appendChild(refreshBtn)
+    } else{
+
+        if (playerchoice == computerChoice){
+            result =  "Its a tie haha!"
+            return result
+        }
+    
+        if( playerchoice == allChoices[0]){
+            if(computerChoice == allChoices[1]){
+                cp = cp + 1
+                result =  "You Lose! Paper beats Rock!"
+            }else{
+                pp = pp + 1
+                result =  "You Won! rock beats scissors"
+            }
+        }
+        
+        if(playerchoice == allChoices[1]){
+            if(computerChoice == allChoices[0]){
+                pp = pp + 1
+                result =  "You won! paper beats rock!"
+            }else{
+                cp = cp + 1
+                result =  "You Lose! scissors beats paper"
+            }
+        }
+        // const allChoices = ['rock','paper','scissors']
+        if(playerchoice == allChoices[2]){
+            if(computerChoice == allChoices[0]){
+                cp = cp + 1
+                result =  "You Lose! rock beats scissors!"
+            }else if(computerChoice == allChoices[1]){
+                pp = pp + 1
+                result =  "You won! Scissors beats paper"
+            }
+        }
     }
 
-    if( playerchoice == allChoices[0]){
-        if(computerChoice == allChoices[1]){
-            cp = cp + 1
-            return "You Lose! Paper beats Rock!"
-        }else{
-            pp = pp + 1
-            return "You Won! rock beats scissors"
-        }
-    }
-    
-    if(playerchoice == allChoices[1]){
-        if(computerChoice == allChoices[0]){
-            pp = pp + 1
-            return "You won! paper beats rock!"
-        }else{
-            cp = cp + 1
-            return "You Lose! scissors beats paper"
-        }
-    }
-    // const allChoices = ['rock','paper','scissors']
-    if(playerchoice == allChoices[2]){
-        if(computerChoice == allChoices[0]){
-            cp = cp + 1
-            return "You Lose! rock beats scissors!"
-        }else if(computerChoice == allChoices[1]){
-            pp = pp + 1
-            return "You won! Scissors beats paper"
-        }
-    }
+
+
 }
 
+// function determineWinner(){
+//     res.setAttribute('id','winner')
+//     console.log(res)
+//     if (pp == 5){
+//         result = "You Won yay!"
+//     }else if (cp == 5){
+//         result = "Dumb human, You loose today!"
+//     }
+// }
 
 //determine winner of 1 game
 //add the score to the screen
